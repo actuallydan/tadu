ba54ca03a84db1683744ecc0a65c9a4c72574e6d
dlkral <dlkral@millersville.edu>
2017-05-18 15:12:04 -0400

    added ability to click and paint your schedule

--- imports/Schedule.jsx
+++ imports/Schedule.jsx
@@ -17,20 +17,14 @@ export default class Schedule extends TrackerReact(React.Component) {
 		}
 	}
 	modifySchedule(e){
+		e.target.parentElement.style.backgroundColor = "transparent" ? "#1de9b6" : "transparent";
 		let coords = e.target.getAttribute("data-coords");
 		Meteor.call("modifySchedule", coords, (err, res)=>{
 			if(err){
 				swal("Whoops!", err, "error");
-			} else {
-				// console.log("changed schedule");
-			}
+			} 
 		})
 	}
-	// componentDidMount(){
-	// 	if(Session.get("schedule_loaded")){
-	// 		// document.getElementById("schedule-row-04").scrollIntoView();
-	// 	}
-	// }
 	renderComponentActually(){
 		const hours = ["00:00", "01:00", "02:00", "03:00", "04:00", "05:00", "06:00", "07:00", "08:00", "09:00", "10:00", "11:00", "12:00", "13:00", "14:00", "15:00", "16:00", "17:00", "18:00", "19:00", "20:00", "21:00", "22:00", "23:00"];
 
@@ -70,10 +64,12 @@ export default class Schedule extends TrackerReact(React.Component) {
 							return day === "Blank" ? 
 							<td className="header" key={day + "T" + hour}>{moment(hour, "HH:mm").format("h a")}</td>
 							: <td key={day + "T" + hour} 
+							
+							style={{backgroundColor: mySched.schedule[day][hour] === null ? "transparent" : "#1de9b6"}}
+							><div draggable="true"
 							data-coords={day + "T" + hour} 
 							onClick={this.modifySchedule.bind(this)}
-							style={{backgroundColor: mySched.schedule[day][hour] === null ? "transparent" : "#1de9b6"}}
-							>{'\u00A0'}</td>
+							onDragEnter={this.modifySchedule.bind(this)}>{'\u00A0'}</div></td>
 						}
 						)
 					}
