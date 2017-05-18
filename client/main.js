8972bd4b25701cee47d4e6a17aaaa7bb9ba30b3e
Dan Kral <dan_kral@yahoo.com>
2017-05-17 23:12:52 -0400

    bug fix

--- client/main.js
+++ client/main.js
@@ -1,6 +1,9 @@
+import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import {render} from 'react-dom';
 import App from '../imports/App.jsx';
 
 
+Meteor.startup(() => {
 	render(<App />, document.getElementById("root"));
+});
